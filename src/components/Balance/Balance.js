58df6a08b36aff1bcd7977a2a15cd06fbcc45434
Vitalii-Mamchur <vetalpatsi@gmail.com>
2021-10-03 17:30:22 +0300

    add mamchur-branch

--- src/components/Balance/Balance.js
+++ src/components/Balance/Balance.js
@@ -1,15 +1,20 @@
 import React, { useState } from "react";
-import styles from "./Balance.module.css";
+import styles from "./Balance.module.scss";
 
 function Balance() {
   const [balance, setBalance] = useState(0);
 
+  function updateBalance() {
+    setBalance(balance + 1, console.log(balance));
+  }
   return (
     <div className={styles.container}>
       <div className={styles.balance}>
         <p className={styles.balanceTitle}>Баланс:</p>
         <p className={styles.balanceCount}>{balance} UAN</p>
-        <button className={styles.balanceBtn}>ПОДТВЕРДИТЬ</button>
+        <button className={styles.balanceBtn} onClick={updateBalance}>
+          ПОДТВЕРДИТЬ
+        </button>
       </div>
     </div>
   );
