3b9d569c19c0cfbb86267b90fcba93f4c3b29485
Iryna Dagli <dagliirina@ukr.net>
2021-10-03 18:36:32 +0300

    add bg

--- src/App.js
+++ src/App.js
@@ -1,26 +1,11 @@
 import React from 'react';
-//import logo from './logo.svg';
-import styles from './base/main.scss';
+import HomeView from './views/Home/HomeView';
 
-function App() {
-  return (
-    <div className={styles.App}>
-      <header className={styles.AppHeader}>
-        {/* <img src={logo} className={styles.App-logo} alt="logo" /> */}
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className={styles.AppLink}
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+export default function App() {
 
-export default App;
+    return (
+     <>   
+     <HomeView />     
+     </>
+    );
+  }
