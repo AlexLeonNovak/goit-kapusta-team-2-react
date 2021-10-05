7917688b581546b8976b553d5ea134237bce11e9
Volodymyr Viter <mad.tvido@gmail.com>
2021-10-05 22:10:06 +0300

    add tabs
    and fixing comments

--- src/App.js
+++ src/App.js
@@ -1,24 +1,30 @@
-import Auth from './pages/Auth/Auth';
+import Auth from "./pages/Auth/Auth";
 import AuthForm from "./components/AuthForm/AuthForm";
-import IncomeList from "./components/income/incomeList";
-import ExpenseList from "./components/expense/expenseList";
-import HomeView from './views/Home/HomeView';
+import IncomeList from "./components/Income/IncomeList";
+import ExpenseList from "./components/Expense/ExpenseList";
+import HomeView from "./views/Home/HomeView";
 import Balance from "./components/Balance";
+import Tabs from "./components/Tabs/Tabs";
 
 // TODO видалити локальні TODO, коли буде BACK
-import IncomeApi from './components/income/api.json';
-import expenseApi from './components/expense/api.json';
-
+import IncomeApi from "./components/Income/api.json";
+import ExpenseApi from "./components/Expense/api.json";
 
 function App() {
   return (
     <>
-    <Auth />
-	    <HomeView />
+      <Auth />
+      <HomeView />
       <AuthForm />
       <Balance />
-      <IncomeList items={IncomeApi} />
-      <ExpenseList items={expenseApi} />
+      <Tabs>
+        <div label="Доход">
+          <IncomeList items={IncomeApi} />
+        </div>
+        <div label="Расход">
+          <ExpenseList items={ExpenseApi} />
+        </div>
+      </Tabs>
     </>
   );
 }
