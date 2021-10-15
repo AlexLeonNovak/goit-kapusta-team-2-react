54c4348182af8e6ededdeb0cf182a3d9e2bd3a8f
Alex Novak <alexleonnovak@gmail.com>
2021-10-15 15:30:57 +0300

    fix menu

--- src/components/Nav/Nav.js
+++ src/components/Nav/Nav.js
@@ -14,43 +14,47 @@ import classNames from 'classnames';
 export default function Navigation() {
 	const [isMenuOpened, setIsMenuOpened] = useState(false);
 
-	const buttonMenuToggle = () => {
-		setIsMenuOpened(!isMenuOpened)
+	const openMenu = () => {
+		setIsMenuOpened(true)
+	}
+
+	const closeMenu = () => {
+		setIsMenuOpened(false)
 	}
 
 	return (
 		<nav className={s.menuWrapper}>
 
-			<button className={s.menuBtnToggle} onClick={buttonMenuToggle}>
+			<button className={s.menuBtnToggle} onClick={openMenu}>
 				<span className={s.burgerLine}/>
 				<span className={s.burgerLine}/>
 				<span className={s.burgerLine}/>
 			</button>
-			<div className={classNames(s.backdrop, {[s.open]: isMenuOpened})} onClick={buttonMenuToggle}/>
+			<div className={classNames(s.backdrop, {[s.open]: isMenuOpened})} onClick={closeMenu}/>
 
 			<NavLink className={s.logoLink} to="/">
 				<div><img src={logo} alt="Logo"/></div>
 			</NavLink>
 			<ul className={classNames(s.menu, {[s.open]: isMenuOpened})}>
 				<li className={s.closeButtonItem}>
-					<button className={s.closeButton} onClick={buttonMenuToggle}>
+					<button className={s.closeButton} onClick={closeMenu}>
 						<svg width="30" height="30" className={s.closeImg}>
 							<use href={sprite + '#icon-close'}/>
 						</svg>
 					</button>
 				</li>
 				<li className={s.menuItem}>
-					<NavLink className={s.menuLink} to={routes.transactions} onClick={buttonMenuToggle}>
+					<NavLink className={s.menuLink} to={routes.transactions} onClick={closeMenu}>
 						<span>Транзакции</span>
 					</NavLink>
 				</li>
 				<li className={s.menuItem}>
-					<NavLink className={s.menuLink} to={routes.categories} onClick={buttonMenuToggle}>
+					<NavLink className={s.menuLink} to={routes.categories} onClick={closeMenu}>
 						<span>Категории</span>
 					</NavLink>
 				</li>
 				<li className={s.menuItem}>
-					<NavLink className={s.menuLink} to={routes.report} onClick={buttonMenuToggle}>
+					<NavLink className={s.menuLink} to={routes.report} onClick={closeMenu}>
 						<span>Отчет</span>
 					</NavLink>
 				</li>
