674fe524f8f83e5b5dd674fb5ef4e5e7a3828141
Tania Tatula <tgrogol@gmail.com>
2021-10-17 00:18:53 +0300

    adds a modal to transactions

--- src/components/TransactionTable/TransactionTable.js
+++ src/components/TransactionTable/TransactionTable.js
@@ -1,62 +1,94 @@
-import React, {useCallback} from 'react';
-import PropTypes from 'prop-types';
-import {useDispatch, useSelector} from 'react-redux';
-import {transactionsOperations, transactionsSelectors} from '../../redux/transactions';
+import React, { useCallback, useState } from "react";
+import PropTypes from "prop-types";
+import { useDispatch, useSelector } from "react-redux";
+import {
+  transactionsOperations,
+  transactionsSelectors,
+} from "../../redux/transactions";
 
-import './TransactionTable.css';
-import s from '../CategoriesList/CategoriesList.module.scss';
-import trash from '../../base/images/svg_black/trash.svg';
-import {categoryTypes} from '../../helpers/constants';
+import "./TransactionTable.css";
+import s from "../CategoriesList/CategoriesList.module.scss";
+import trash from "../../base/images/svg_black/trash.svg";
+import { categoryTypes } from "../../helpers/constants";
+import Modal from "../Modal";
 
-export const TransactionTable = ({type}) => {
-	const dispatch = useDispatch();
+export const TransactionTable = ({ type }) => {
+  const dispatch = useDispatch();
+  const [showModal, setShowModal] = useState(false);
+  const [id, setId] = useState("");
 
-	const transactions = useSelector(transactionsSelectors.getAllTransactions)
-		.filter(transaction => transaction.category.type === type);
+  const toggleModal = () => {
+    setShowModal(!showModal);
+    setId("");
+  };
+  const onOpenModal = (id) => {
+    setShowModal(true);
+    setId(id);
+  };
 
-	const onDeleteTransaction = useCallback(
-		id => {
-			dispatch(transactionsOperations.deleteTransaction(id));
-		},
-		[dispatch],
-	);
+  const transactions = useSelector(
+    transactionsSelectors.getAllTransactions
+  ).filter((transaction) => transaction.category.type === type);
 
-	return (
-		<table className="income">
-			<thead className="income__head">
-			<tr>
-				<th>Дата</th>
-				<th>Описание</th>
-				<th>Категория</th>
-				<th>Сумма</th>
-				<th/>
-			</tr>
-			</thead>
+  const onDeleteTransaction = useCallback(
+    (id) => {
+      dispatch(transactionsOperations.deleteTransaction(id));
+    },
+    [dispatch]
+  );
 
-			<tbody>
-			{transactions.map(
-				({_id, datetime, description, category, amount}) => (
-					<tr key={_id}>
-						<td>{datetime}</td>
-						<td>{description}</td>
-						<td>{category.name}</td>
-						<td>{type === categoryTypes.EXPENSE && '-'}{amount}</td>
-						<td align="center" className={s.categoriesActions}>
-							<button onClick={() => onDeleteTransaction(_id)}
-							        className={s.categoriesActionsDelete}
-							>
-								<img src={trash} alt="Delete" className={s.categoriesActionsDeleteIcon}/>
-							</button>
-						</td>
-					</tr>
-				)
-			)}
-			</tbody>
-		</table>
-	);
+  return (
+    <table className='income'>
+      <thead className='income__head'>
+        <tr>
+          <th>Дата</th>
+          <th>Описание</th>
+          <th>Категория</th>
+          <th>Сумма</th>
+          <th />
+        </tr>
+      </thead>
+
+      <tbody>
+        {transactions.map(
+          ({ _id, datetime, description, category, amount }) => (
+            <tr key={_id}>
+              <td>{datetime}</td>
+              <td>{description}</td>
+              <td>{category.name}</td>
+              <td>
+                {type === categoryTypes.EXPENSE && "-"}
+                {amount}
+              </td>
+              <td align='center' className={s.categoriesActions}>
+                <button
+                  onClick={() => onOpenModal(_id)}
+                  className={s.categoriesActionsDelete}
+                >
+                  <img
+                    src={trash}
+                    alt='Delete'
+                    className={s.categoriesActionsDeleteIcon}
+                  />
+                </button>
+              </td>
+            </tr>
+          )
+        )}
+      </tbody>
+
+      {showModal && (
+        <Modal
+          ChildComponent
+          title={"Вы уверены?"}
+          onClose={toggleModal}
+          onClick={() => onDeleteTransaction(id)}
+        />
+      )}
+    </table>
+  );
 };
 
 TransactionTable.propTypes = {
-	type: PropTypes.oneOf([categoryTypes.EXPENSE, categoryTypes.INCOME])
-}
-
+  type: PropTypes.oneOf([categoryTypes.EXPENSE, categoryTypes.INCOME]),
+};
